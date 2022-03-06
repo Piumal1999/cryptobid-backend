44199d5bed7b97b28f7b3f321c3ad842b0290482
piumal1999 <piumalrathnayake@hotmail.com>
2022-03-06 22:08:23 +0530

    Initialize Springboot

---
+++ src/main/java/com/cryptobid/backend/CryptoBidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cryptobid.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBidBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptobid/backend/CryptoBidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptobid.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
