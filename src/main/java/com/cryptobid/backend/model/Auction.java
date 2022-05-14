49310af3793a288498b1a71f20d667611d0f8cbe
Konara98 <lakshithakonara3@gmail.com>
2022-05-14 21:14:59 +0530

    Create auction entitiy

---
+++ src/main/java/com/cryptobid/backend/model/Auction.java
@@ -0,0 +1,76 @@
+package com.cryptobid.backend.model;
+
+import com.cryptobid.backend.util.UserType;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Temporal;import javax.persistence.Id;
+
+@Entity
+public class Auction {
+
+	@Id
+	@GeneratedValue
+	@Column(nullable = false)
+	private int id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Date startTime;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
+	private Date endTime;
+
+	@Column(nullable = false)
+	private int initialValue;
+
+	@Column
+	private String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getInitialValue() {
+		return initialValue;
+	}
+
+	public void setInitialValue(int initialValue) {
+		this.initialValue = initialValue;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
