6191480f0472115c098a4095a1923a943a43c37b
piumal1999 <piumalrathnayake@hotmail.com>
2022-05-16 15:57:43 +0530

    Create application.yml.example

--- src/main/java/com/cryptobid/backend/controller/admin/AuctionController.java
+++ src/main/java/com/cryptobid/backend/controller/admin/AuctionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("AdminAuctionController")
 @RequestMapping("/api/admin/auctions")
 public class AuctionController {
 	private final AuctionService auctionService;
--- src/main/java/com/cryptobid/backend/controller/admin/CryptocurrencyController.java
+++ src/main/java/com/cryptobid/backend/controller/admin/CryptocurrencyController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("AdminCryptocurrencyController")
 @RequestMapping("/api/admin/cryptocurrency")
 public class CryptocurrencyController {
 
