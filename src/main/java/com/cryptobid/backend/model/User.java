2fa468710dcc22ed9a3549b392559481ad4cede9
Konara98 <lakshithakonara3@gmail.com>
2022-05-16 12:30:10 +0530

    Add getters and setters

--- src/main/java/com/cryptobid/backend/model/User.java
+++ src/main/java/com/cryptobid/backend/model/User.java
@@ -6,13 +6,18 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 
 	@Id
+	@GeneratedValue
 	@Column(nullable = false)
+	private int id;
+
+	@Column(nullable = false,unique = true)
 	private String username;
 
 	@Column(nullable = false)
@@ -30,4 +35,60 @@ public class User {
 	@Enumerated(EnumType.STRING)
 	@Column(length = 8, nullable = false)
 	private UserType type;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public float getTotalBalance() {
+		return totalBalance;
+	}
+
+	public void setTotalBalance(float totalBalance) {
+		this.totalBalance = totalBalance;
+	}
+
+	public UserType getType() {
+		return type;
+	}
+
+	public void setType(UserType type) {
+		this.type = type;
+	}
 }
