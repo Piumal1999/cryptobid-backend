b95b9cb6b5ed9083ad9a14515383d661dc5ea315
Piumal Rathnayake <piumalrathnayake@hotmail.com>
2022-05-16 19:43:29 +0530

    Create endpoint for start an auction (#17)

--- src/main/java/com/cryptobid/backend/controller/admin/AuctionController.java
+++ src/main/java/com/cryptobid/backend/controller/admin/AuctionController.java
@@ -3,12 +3,16 @@ package com.cryptobid.backend.controller.admin;
 import com.cryptobid.backend.exceptions.ResourceNotFoundException;
 import com.cryptobid.backend.model.Auction;
 import com.cryptobid.backend.model.Bid;
+import com.cryptobid.backend.model.User;
 import com.cryptobid.backend.service.AuctionService;
 import com.cryptobid.backend.util.AuctionStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,11 +35,17 @@ public class AuctionController {
 		return auctionService.getBidsByAuctionId(id);
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Auction startAuction(@RequestBody Auction auction,
+								@AuthenticationPrincipal User user) {
+		return auctionService.startAuction(auction, user.getId());
+	}
+
 	@PutMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Auction endOrCancelAuction(@PathVariable int id,
 									  @RequestParam AuctionStatus auctionStatus) throws ResourceNotFoundException {
 		return auctionService.endOrCancelAuction(id, auctionStatus);
 	}
-
 }
