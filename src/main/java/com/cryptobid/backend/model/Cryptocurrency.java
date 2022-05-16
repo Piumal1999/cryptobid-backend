31176dca9020f70f33b4f9e335e655de338b732e
Konara98 <97779686+Konara98@users.noreply.github.com>
2022-05-16 12:32:47 +0530

    Add getters and setters (#5)

--- src/main/java/com/cryptobid/backend/model/Cryptocurrency.java
+++ src/main/java/com/cryptobid/backend/model/Cryptocurrency.java
@@ -2,13 +2,18 @@ package com.cryptobid.backend.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Cryptocurrency {
 
 	@Id
+	@GeneratedValue
 	@Column(nullable = false)
+	private int id;
+
+	@Column(nullable = false, unique = true)
 	private String symbol;
 
 	@Column(nullable = false)
@@ -17,4 +22,35 @@ public class Cryptocurrency {
 	@Column(nullable = false)
 	private int rank;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
 }
